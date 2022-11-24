107c1e4279c7f3e4c4b99dd4d136d7196509c671
Ernesto Hernández <ernesto@nimbo.mx>
2022-11-24 10:51:38 -0600

    Initial commit

---
+++ .next/static/webpack/static/development/pages/productos.js.3859c2d943abce4fa4fd.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("static/development/pages/productos.js",{
+
+/***/ 4:
+false,
+
+/***/ 5:
+/*!*******************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fproductos&absolutePagePath=%2FUsers%2Fernestohernandez%2FNimbo%2FCRM%2Fcrmcliente%2Fpages%2Fproductos.js ***!
+  \*******************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fproductos&absolutePagePath=%2FUsers%2Fernestohernandez%2FNimbo%2FCRM%2Fcrmcliente%2Fpages%2Fproductos.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fproductos&absolutePagePath=%2FUsers%2Fernestohernandez%2FNimbo%2FCRM%2Fcrmcliente%2Fpages%2Fproductos.js!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=productos.js.3859c2d943abce4fa4fd.hot-update.js.map
